8e4dff115c42a99379340ef0b373259577517300
Sergey Boyko <boiko.s.wezom@gmail.com>
2020-02-07 09:28:55 +0200

    Added index.html;

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -1,4 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath))
+
+const index = path.resolve(publicPath, 'index.html')
+
+app.use((req, res, next) => {
+    res.sendFile(index)
+})
 
 module.exports = app
