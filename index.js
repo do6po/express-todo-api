f35f44b68c7c0dbc12b473827f4c18de031d69e4
Sergey Boyko <boiko.s.wezom@gmail.com>
2020-02-08 11:29:38 +0200

    Added sequelize;

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const app = require('./bootstrap/app')
 const routes = require('./routes/main')
+const sequelize = require('./bootstrap/database')
 
 require('dotenv').config()
 
 app.use('/api/todo', routes)
 
-console.log(`${process.env.URL}:${process.env.PORT}`)
-app.listen(process.env.PORT)
+async function start() {
+    try {
+        await sequelize.sync()
+        app.listen(process.env.PORT)
+        console.log(`${process.env.URL}:${process.env.PORT}`)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
+
+
+
